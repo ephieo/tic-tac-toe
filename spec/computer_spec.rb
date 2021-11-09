a27d8e05d355c19a4b7df63bc91e36134f114202
Ephie Oyedoh <ephieoyedoh@Ephies-MacBook-Pro-2.local>
2021-11-09 10:39:43 +0000

    removing commented out code

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -13,9 +13,6 @@ require_relative '../lib/game_mode'
 describe Computer do
   locations = %w[x o 3 x o o o x x]
   let(:board) { Board.new(locations) }
-  # let(:game_phrases) { GamePhrases.new }
-  # let(:io) { InputOutput.new(board.board_locations, game_phrases) }
-  # let(:rules) { GameRules.new(io, game_phrases) }
   let(:game_mode) { GameMode.new(board) }
   subject(:computer) { described_class.new('o', 'Computer', board) }
 
@@ -23,8 +20,8 @@ describe Computer do
     expect(computer.marker).to eq('o')
   end
   it ' returns the computer player\'s choice as an integer' do
-    # io = InputOutput.new(board.board_locations, game_phrases)
     allow(game_mode.input_output).to receive(:gets).and_return('3')
+
     expect(computer.play_game).to be_a_kind_of(String)
   end
 end
